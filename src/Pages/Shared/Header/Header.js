663edc5f9082de9d0075f0bcc118df95f297287b
rokib97 <hasanrokib1997@gmail.com>
2022-05-06 15:44:19 +0600

    added navbar with active route

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,63 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../Images/banner-logo.png";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h2>Header</h2>
-    </div>
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        sticky="top"
+        className="shadow-lg"
+        variant="light"
+      >
+        <Container>
+          <NavLink to="/">
+            <img width={60} src={logo} alt="" />
+          </NavLink>
+          <Navbar.Brand
+            as={Link}
+            to="/"
+            className="ms-2 title fw-bolder header-title"
+          >
+            INFINITY ELECTRONICS GADGETS
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto navbar-color">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "active-link" : "link"
+                }
+                to={"/"}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "active-link" : "link"
+                }
+                to={"/blogs"}
+              >
+                Blogs
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "active-link" : "link"
+                }
+                to={"/login"}
+              >
+                Login
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
